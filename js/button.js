99b299ce10f667717a51840db489bddc214c1fab
vgeorgiev0 <valentingeorgiev1996@gmail.com>
2021-05-26 01:20:54 +0300

    Add YouTube link validation

--- js/button.js
+++ js/button.js
@@ -2,10 +2,13 @@ const goBtn = document.getElementById('go-btn');
 const youtubeLink = document.getElementById('youtube-link');
 
 youtubeLink.addEventListener('keyup', (event) =>{
-    const value = event.currentTarget.value;
+    let url = event.currentTarget.value;
+    if (url != undefined || url != '') {
+        let regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
+        let match = url.match(regExp);
+        if (match && match[2].length == 11)
         goBtn.disabled = false;
-
-        if (value === "") {
-            goBtn.disabled = true;
-        }
+    } else {
+        goBtn.disabled = true;
+    }
 });
