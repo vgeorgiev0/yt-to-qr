fd51efe527baef8943364be4f7ed52ab6cf388d3
vgeorgiev0 <valentingeorgiev1996@gmail.com>
2021-05-25 16:23:23 +0300

    Add Contextual menu to the project

--- main.js
+++ main.js
@@ -1,28 +1,76 @@
-const  { app, BrowserWindow } = require('electron')
-const path = require = require('path')
+const electron = require('electron');
 
-function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
-    })
-    win.loadFile('index.html')
+const url = require('url');
+const path = require('path');
+
+
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+
+// Listen for app to be ready
+app.on('ready', function(){
+  // Create new window
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    minWidth: 800,
+    height: 800,
+    minHeight: 600,
+  });
+  // Load html into window
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  // Build menu from template
+const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+// Insert menu
+Menu.setApplicationMenu(mainMenu);
+});
+
+// Create menu template// Module to control application
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu:[
+    {
+      label: 'Refresh'
+      
+    },
+    {
+      label: 'Quit',
+      accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+      click(){
+        app.quit();
+      }
+    },
+  ]
+  }
+];
+
+// If mac add an empty object to menu
+if(process.platform == 'darwin'){
+  mainMenuTemplate.unshift({});
 }
 
-app.whenReady().then(() => {
-    createWindow()
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
+// Add developer tools item if not in prod
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu:[
+      {
+        label:'DevTools',
+        accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
         }
-    })
-})
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin'){
-        app.quit()
-    }
-})
-
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  })
+}
